e3d5cf3a4a69ca1ebf93d7783a02ccabd7ee95db
Rich Hauck <rich@hauckinteractive.com>
2026-04-16 01:46:07 -0400

    WIP - Added projectData to store

--- src/stores/useProjectsStore.ts
+++ src/stores/useProjectsStore.ts
@@ -1,16 +1,28 @@
-import type { Project } from "#/types/project";
+import type { Project, ProjectData } from "#/types/project";
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 interface useProjectsStore {
   projects: Project[];
+  projectData: Record<string, ProjectData>;
   setProjects: (projects: Project[]) => void;
+  setProjectDataById: (id: string, data: ProjectData) => void;
+  getProjectDataById: (id: string) => ProjectData | undefined;
 }
 const storePrefix = "useProjectsStore";
 const useProjectsStore = create<useProjectsStore>()(
   devtools(
-    (set) => ({
+    (set, get) => ({
       projects: [],
+      projectData: {},
       setProjects: (projects) => set({ projects }),
+      setProjectDataById: (id, data) =>
+        set((state) => ({
+          projectData: {
+            ...state.projectData,
+            [id]: { ...state.projectData[id], ...data },
+          },
+        })),
+      getProjectDataById: (id) => get().projectData[id],
     }),
     { name: `${storePrefix}` },
   ),
